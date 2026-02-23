1fdbfb8eec2daea6377e5273787bc37dcc448660
mandeyatayt-ux <mandeya.tayt@gmail.com>
2026-02-23 15:50:56 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import TheUndercut from './TheUndercut.jsx'
-import './index.css'
+import TheUndercut from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <TheUndercut />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
