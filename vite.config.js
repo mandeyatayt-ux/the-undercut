85938c3e94df8d84d7302b30c32f4bbe9e6722e6
mandeyatayt-ux <mandeya.tayt@gmail.com>
2026-02-23 15:25:47 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,24 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: false,
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: (assetInfo) => {
+          const info = assetInfo.name.split('.')
+          const ext = info[info.length - 1]
+          if (/\.(png|jpe?g|gif|svg|webp|ico)$/i.test(assetInfo.name)) {
+            return 'assets/images/[name]-[hash][extname]'
+          }
+          return 'assets/[name]-[hash][extname]'
+        },
+      },
+    },
+  },
+  server: {
+    port: 3000,
+    open: true,
   },
 })
